4cbe9829d1f898f6adaf8c60362839ccb8c9f140
Mateusz Żółtak <zozlak@zozlak.org>
2016-04-07 11:07:26 +0200

    Dirty commit to checkout other branch

--- php/import.php
+++ php/import.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * Sample import script utilizing import\Document class.
+ * Sample import script utilizing model\Document class.
  * 
  * To run it:
  * - assure proper database connection settings in config.inc.php
  * - set up configuration variables in lines 14-25
  * - run script from the command line 'php import.php'
  */
-require_once 'src/utils/ClassLoader.php';
-new utils\ClassLoader();
+require_once 'src/util/ClassLoader.php';
+new util\ClassLoader();
 require_once 'config.inc.php';
 
 // token iterator class; if you do not know it, set to NULL
-$iterator = import\Document::XML_READER;
+$iterator = model\Document::DOM_DOCUMENT;
 // if processed data should be stored in the database
 $save = false; 
 // allows to limit number of processed tokens (put 0 to process all)
@@ -26,26 +26,17 @@ $saveDir = 'docStorage';
 // simply skip broken tokens (true) or break the import on first broken error (false)
 $skipErrors = true;
 
-//$schemaPath = '../sample_data/testtext-schema.xml';
-//$dataPath   = '../sample_data/testtext.xml';
-//$schemaPath = '../sample_data/testcases-rm-toks-schema.xml';
-//$dataPath   = '../sample_data/testcases-rm-toks.xml';
-//$schemaPath = '../sample_data/SwissProt-schema.xml';
-//$dataPath   = '../sample_data/SwissProt.xml';
-//$schemaPath = '../sample_data/baffleLex_v_0.2_zmorge_20151002-schema.xml';
-//$dataPath   = '../sample_data/baffleLex.xml';
-
 ###########################################################
 
-$PDO = new \PDO($CONFIG['dbConn'], $CONFIG['dbUser'], $CONFIG['dbPasswd']);
+$PDO = new \PDO($CONFIG['dbConn']);
 $PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 $PDO->beginTransaction();
 
 $name = explode('/', $dataPath);
 $name = array_pop($name);
-$pb = new utils\ProgressBar(null, 10);
+$pb = new util\ProgressBar(null, 10);
 
-$doc = new import\Document($PDO);
+$doc = new model\Document($PDO);
 $doc->loadFile($dataPath, $schemaPath, $name, $iterator);
 $doc->save($saveDir, $limit, $pb, $skipErrors);
 
